21d9829c5b67cbbcd06a08ecb5a558ec1f445e92
Matheus, Santos <matheusinacio.bs@gmail.com>
2023-09-27 19:10:40 -0300

    fix tacking button

--- commons/tasks/email.py
+++ commons/tasks/email.py
@@ -8,7 +8,9 @@ def send_email(recipient_list: list, email_preset: dict = {}):
     subject = email_preset.get('subject', 'Assunto do E-mail')
     message = email_preset.get('message', 'Conteúdo do E-mail')
     from_email = EMAIL_HOST_USER
-
-    send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    try:
+        send_mail(subject, message, from_email, recipient_list, fail_silently=False)
+    except Exception as ex:
+        print(ex)
 
     return HttpResponse('E-mail enviado com sucesso!')
