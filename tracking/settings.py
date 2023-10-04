2a004519a12304b139e96fbdf11bccd8f97c0923
Matheus Inácio <matheusinacio.bs@gmail.com>
2023-10-04 16:44:55 -0300

    Update settings.py

--- tracking/settings.py
+++ tracking/settings.py
@@ -157,7 +157,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static/')
+    os.path.join(BASE_DIR, 'static/'),
+    STATIC_ROOT
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = '/media/'
